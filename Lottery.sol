62e0a426a7a56adc81358fe9c0d19fa38462ffb0
Zodigram <37299909+Zodigram@users.noreply.github.com>
2020-12-08 18:17:56 -0500

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -7,43 +7,55 @@ contract Lottery {
     uint WeightedLottoFunds; 		// funds up for grabs in weighted lotto
     uint RRouletteLottoFunds; 		// funds up for grabs in russian roulette lotto
     uint RandomRRouletteLottoFunds; 	// funds up for grabs in the random russian roulette
+	
     uint GuessingLottoTarget; 		// current guessing target in guessing lotto
     uint RRouletteLottoTarget; 		// current guessing target in russian roulette
     uint RandomRRouletteLottoTarget; 	// current guessing target in russian roulette with random elimination
+    
+    uint GuessingLottoDifficulty; 	//current guessing difficulty for Guessing Lotto
+    uint RRouletteLottoDifficulty;	//current guessing difficulty for Russian Roulette Lotto
+    uint RandomRRouletteLottoDifficulty; //current guessing difficulty for Randomized Russian Roulette Lotto
 
     address[] participantsMain;  	// this holds all participant addresses in the main lotto
-    address[] participantsWeighted;  	// this holds all participant addresses in the main lotto
-    mapping(address => uint) public weights;  	// This stores each participants betting weight
+    address[] participantsWeighted;  	// this holds all participant addresses in the weighted lotto
+    mapping(address => uint) public weights;  	// This stores each participants betting weight in the weighted lotto
     address[] EliminatedParticipantsRRoulette;  //This holds a list of all participants barred from the Russian roulette lotto
     mapping(address => int) public EliminatedStatus; //This tracks whether a participant is banned from the russian roulette lotto or not. By default, each address is mapped to 0, which is acceptable. When an address guesses too poorly, it is switched to -1.
-    address[] EliminatedParticipantsRandomRRoulette; //This holds a list of all participants barred from the Russian roulette lotto
-    mapping(address => uint) public RandomEliminatedStatus; //This tracks whether a participant is banned from the russian roulette lotto or not. By default, each address is mapped to 0, which is acceptable. When an address guesses too poorly, it is switched to -1.
+    address[] EliminatedParticipantsRandomRRoulette; //This holds a list of all participants barred from the randomized Russian roulette lotto
+    mapping(address => uint) public RandomEliminatedStatus; //This tracks whether a participant is banned from the randomized russian roulette lotto or not. By default, each address is mapped to 0, which is acceptable. When an address guesses too poorly, it is switched to -1.
     address public lotteryManager;     			    // this is the person in charge of the lottery
 
     constructor() public {
-        lotteryManager=msg.sender;
-    	MainLottoFunds=0;
-    	UpdateGuessingLottoTarget();
-    	UpdateRRouletteLottoTarget();
-    	UpdateRandomRRouletteLottoTarget();
+        lotteryManager=msg.sender; //set the owner to the contract creator
+    	MainLottoFunds=0; //no funds at first in Main Lotto
+	GuessingLottoFunds=0; //no funds at first in Guessing Lotto
+	WeightedLottoFunds=0; //no funds at first in Weighted Lotto
+	RRouletteLottoFunds=0; //no funds at first in Russian Roulette Lotto
+	RandomRRouletteLottoFunds=0; //no funds at first in Randomized Russian Roulette Lotto
+    	UpdateGuessingLottoTarget(); //create initial target for Guessing lotto
+    	UpdateRRouletteLottoTarget(); //create initial target for Russian Roulette Lotto
+    	UpdateRandomRRouletteLottoTarget(); //create initial target for Randomized Russian Roulette Lotto
+	GuessingLottoDifficulty = 10;
+	RRouletteLottoDifficulty = 10;
+	RandomRRouletteLottoDifficulty=10;
     }
     
-      function getOwner() public view returns (address) {
-        return lotteryManager;
+      function getOwner() public view returns (address) { //check who the owner is
+        return lotteryManager; 
     } 
     
-    function random() private view returns(uint) {
+    function random() private view returns(uint) { //generate "random" number
         return uint(keccak256(abi.encodePacked(block.difficulty, now, participantsMain)));
     }
      
-    function MainLottoEntry() public payable {
-	require(msg.value == 1 ether, "Please pay exactly one ether");	//changed msg.sender to msg.value
+    function MainLottoEntry() public payable { //Pay 1 eth to be added to pool of competitors
+	require(msg.value == 1 ether, "Please pay exactly one ether");
 	participantsMain.push(msg.sender);
-	MainLottoFunds+=1000000000000000000;
+	MainLottoFunds+=msg.value; //add 1 eth to pot
     }
     
-    function MainLottoEnd() public payable returns(uint) {
-        require(msg.sender == lotteryManager);
+    function MainLottoEnd() public payable returns(uint) { //Pay out winner of Main Lotto
+        require(msg.sender == lotteryManager); //can only be executed by owner
 	    uint winning_index = random() % participantsMain.length; 	//randomly generate winner
 	    participantsMain[winning_index].transfer(MainLottoFunds); 	// send funds to winner
 	    MainLottoFunds=0; 						// reset fund counter
@@ -51,135 +63,156 @@ contract Lottery {
 	    participantsMain = new address[](0);
     }    
     
-    function getMainLottoPlayers() public view returns(address[]) {
+    function getMainLottoPlayers() public view returns(address[]) { //See who is participating in the Main Lotto
         return participantsMain;
     } 
 
-    function getMainLottoFunds() public view returns(uint) {
+    function getMainLottoFunds() public view returns(uint) { //See how much eth is up for grabs in the Main Lotto
         return MainLottoFunds;
     } 
     
-    function GuessingLottoEntry(uint guess) public payable {
-        require(msg.value == 1 ether, "Please pay exactly one ether");
-        GuessingLottoFunds+=1;
-        if (guess==GuessingLottoTarget) {
-            msg.sender.transfer(GuessingLottoFunds);
-            GuessingLottoFunds=0;
+    function GuessingLottoEntry(uint guess) public payable { //Pay one eth for chance to guess target and win pot
+        require(msg.value == 1 ether, "Please pay exactly one ether"); 
+        GuessingLottoFunds+=msg.value; //Add payment to Guessing Lotto pot
+        if (guess==GuessingLottoTarget) { //Only when guess is exactly right
+            msg.sender.transfer(GuessingLottoFunds); //Transfer pot to winner
+            GuessingLottoFunds=0; //Reset pot
         }
-        UpdateGuessingLottoTarget();
+        UpdateGuessingLottoTarget(); //Choose new target every guess to avoid process of elimination
     }
 
-     function UpdateGuessingLottoTarget() private {
-        GuessingLottoTarget=random() % 10;
+     function UpdateGuessingLottoTarget() private { //Choose new guessing target
+        GuessingLottoTarget=random() % GuessingLottoDifficulty; //Restricted to number between 0-9
     }
-    function getGuessingLottoFunds() public view returns(uint){
-        return GuessingLottoFunds;
+    
+    function UpdateRandomRRouletteLottoDifficulty(uint new_difficulty) public { //Update difficulty for Guessing Lotto
+        require(msg.sender == lotteryManager); //Can only be updated by owner of contract
+	GuessingLottoDifficulty=new_difficulty;
     } 
     
-    function WeightedLottoEntry() public payable {
-        weights[msg.sender]=msg.value;
-        participantsWeighted.push(msg.sender);
-        WeightedLottoFunds+=msg.value;
+    function getGuessingLottoFunds() public view returns(uint){ //See how much eth is up for grabs in the Guessing Lotto
+        return GuessingLottoFunds; 
+    } 
+    
+    function WeightedLottoEntry() public payable { //Pay however much you want to enter the Weighted lotto
+    	require(msg.value % 1000000000000000000 = 0, "Please only pay in exact eth"); //Pay exclusively in eth, not wei
+        weights[msg.sender]=msg.value; //Your address is mapped to how much you paid to enter
+        participantsWeighted.push(msg.sender); //You are recorded as a participant
+        WeightedLottoFunds+=msg.value; //The money you paid is added to the pot
     }
     
     function WeightedLottoEnd() public {
-        require(msg.sender == lotteryManager);
-        uint winning_weight = random() % (WeightedLottoFunds / 1000000000000000000);
-        uint current_tally=(weights[participantsWeighted[0]] / 1000000000000000000);
+        require(msg.sender == lotteryManager); //Can only be ended by owner of contract
+        uint winning_weight = random() % (WeightedLottoFunds  / 1000000000000000000); //Choose a random weight (winner contributed weight in this interval)
+        uint current_tally=(weights[participantsWeighted[0]]  / 1000000000000000000); //Identify how much first participant contributed to weight
         uint prev_tally=0;
-        if (winning_weight<=current_tally) {
-            participantsWeighted[0].transfer(WeightedLottoFunds);
+        if (winning_weight<=current_tally) { //See if first participant won
+            participantsWeighted[0].transfer(WeightedLottoFunds); //If so, give them the pot
         }
-        else {
+        else { //Otherwise
             uint i=1;
-            while (i < participantsWeighted.length) {
-                prev_tally = current_tally;
-                current_tally+=(weights[participantsWeighted[i]] / 1000000000000000000);
-                if  (winning_weight >= prev_tally && winning_weight <= current_tally) {                            
-                    participantsWeighted[i].transfer(WeightedLottoFunds);
+	    uint winner_found=0;
+            while (i < participantsWeighted.length && winner_found==0) { //iterate through participants until you find your winner
+                prev_tally = current_tally; //set lower end of interval being checked
+                current_tally+=(weights[participantsWeighted[i]] / 1000000000000000000); //set higher end of interval being checked
+                if  (winning_weight >= prev_tally && winning_weight <= current_tally) { //see if interval user contributed contains winning weight           
+                    participantsWeighted[i].transfer(WeightedLottoFunds); //If so, give them the pot
+		    winner_found=1; //Note that you've already found your winner
                 }
-            i++;
+            i++; //Move on to the next participant
             } 
         }
-        WeightedLottoFunds=0;
-        delete participantsWeighted;
-	    participantsWeighted = new address[](0);
+        WeightedLottoFunds=0; //Reset the pot
+        delete participantsWeighted; //Reset the participants
+	participantsWeighted = new address[](0);
     }    
 
-    function getWeightedLottoPlayers() public view returns(address[]) {
+    function getWeightedLottoPlayers() public view returns(address[]) { //Check who's participating in the Weighted Lotto
         return participantsWeighted;
     } 
     
-    function getWeightedLottoFunds() public view returns(uint) {
-        return WeightedLottoFunds / 1000000000000000000;
+    function getWeightedLottoFunds() public view returns(uint) { See how much eth is up for grabs in the Weighted Lotto
+        return WeightedLottoFunds;
     } 
     
-    function RRouletteEntry(uint guess) public payable {
+    function RRouletteEntry(uint guess) public payable { //Pay 1 eth to participate in the Russian Roulette Lotto
         require(msg.value == 1 ether, "Please pay exactly one ether");
-        require(EliminatedStatus[msg.sender]!=1, "Please wait until you may enter again");
-        RRouletteLottoFunds+=msg.value;
+        require(EliminatedStatus[msg.sender]!=1, "Please wait until you may enter again"); //Cannot participate again if eliminated in this round
+        RRouletteLottoFunds+=msg.value; //Add funds to pot if amount paid is acceptable and participant not eliminated previously
         int target_int=int(RRouletteLottoTarget);
         int guess_int=int(guess);
         int distance_int = (target_int-guess_int);
         if (distance_int < 0) {
             distance_int = 0 - distance_int;
         }
-        distance_int = distance_int % 12;
-        if (guess==RRouletteLottoTarget && EliminatedStatus[msg.sender]!=1) {
-            msg.sender.transfer(RRouletteLottoFunds);
-            RRouletteLottoFunds=0;
-            for (uint i=0;i<EliminatedParticipantsRRoulette.length;i++) {
+        distance_int = distance_int % RRouletteLottoDifficulty; //Distance between guess and Russian Roulette Target
+        if (guess==RRouletteLottoTarget) { //Check if user guessed correctly
+            msg.sender.transfer(RRouletteLottoFunds); //If so, give them the pot
+            RRouletteLottoFunds=0; //Reset the pot
+            for (uint i=0;i<EliminatedParticipantsRRoulette.length;i++) { //Unban all banned participants in preparation for the next round
                 EliminatedStatus[EliminatedParticipantsRRoulette[i]]=0;
             }
-            delete EliminatedParticipantsRRoulette;
-	        EliminatedParticipantsRRoulette = new address[](0);
+            delete EliminatedParticipantsRRoulette; //Clear list of blacklisted addresses
+	    EliminatedParticipantsRRoulette = new address[](0);
         }
-        else if (EliminatedStatus[msg.sender]==0 && distance_int > 2) {
-            EliminatedParticipantsRRoulette.push(msg.sender);
+        else if (distance_int > 2) { //If guess was wrong and it was off by more than 2
+            EliminatedParticipantsRRoulette.push(msg.sender); //You get blacklisted
             EliminatedStatus[msg.sender]=1;
         }
-        UpdateRRouletteLottoTarget();
+        UpdateRRouletteLottoTarget(); //Choose new target every guess to avoid process of elimination
     }
     
-    function UpdateRRouletteLottoTarget() private {
-        RRouletteLottoTarget=random() % 12;
+    function UpdateRRouletteLottoTarget() private { Choose new target for the Russian Roulette lotto
+        RRouletteLottoTarget=random() % RRouletteLottoDifficulty;
     }
 
-    function getRRouletteLottoFunds() public view returns(uint) {
+    function UpdateRRouletteLottoDifficulty(uint new_difficulty) public { //Update difficulty for Russian Roulette
+        require(msg.sender == lotteryManager); //Can only be updated by owner of contract
+	RRouletteLottoDifficulty=new_difficulty;
+    } 
+    
+    function getRRouletteLottoFunds() public view returns(uint) { //See how much is up for grabs in the Russian Roulette lotto
         return RRouletteLottoFunds;
     } 
     
-    function RandomRRouletteEntry(uint guess) public payable {
+    function RandomRRouletteEntry(uint guess) public payable { //Pay 1 eth to enter the Randomized Russian Roulette lotto
         require(msg.value == 1 ether, "Please pay exactly one ether");
         require(RandomEliminatedStatus[msg.sender]!=1, "Please wait until you may enter again");
 
-        RandomRRouletteLottoFunds+=msg.value;
-        if (guess==RandomRRouletteLottoTarget && RandomEliminatedStatus[msg.sender]!=1) {
-            msg.sender.transfer(RandomRRouletteLottoFunds);
-            RandomRRouletteLottoFunds=0;
-            for (uint i=0;i<EliminatedParticipantsRandomRRoulette.length;i++) {
-                RandomEliminatedStatus[EliminatedParticipantsRandomRRoulette[i]]=0;
+        RandomRRouletteLottoFunds+=msg.value; //add payment to pot
+        if (guess==RandomRRouletteLottoTarget) { //If your guess is exactly right
+            msg.sender.transfer(RandomRRouletteLottoFunds); //You get the pot
+            RandomRRouletteLottoFunds=0; //Reset the pot
+            for (uint i=0;i<EliminatedParticipantsRandomRRoulette.length;i++) { //Go through list of eliminated participants
+                RandomEliminatedStatus[EliminatedParticipantsRandomRRoulette[i]]=0; //Remove them from the blacklist
             }
             delete EliminatedParticipantsRandomRRoulette;
 	        EliminatedParticipantsRandomRRoulette = new address[](0);
         }
-        else if (RandomEliminatedStatus[msg.sender]==0 && random() % 6 != 1) {
-            EliminatedParticipantsRandomRRoulette.push(msg.sender);
+        else if (random() % 6 != 1) { 1/6 chance you can try again
+            EliminatedParticipantsRandomRRoulette.push(msg.sender); //if you fail, you're blacklisted
             RandomEliminatedStatus[msg.sender]=1;
         }
-        UpdateRandomRRouletteLottoTarget();
+        UpdateRandomRRouletteLottoTarget(); //Choose new target every guess to avoid process of elimination
     }
     
-    function UpdateRandomRRouletteLottoTarget() private {
-        RandomRRouletteLottoTarget=random() % 3;
+    function UpdateRandomRRouletteLottoTarget() private { Choose new Randomized Russian Roulette number
+        RandomRRouletteLottoTarget=random() % RandomRRouletteLottoDifficulty; //Target based on Difficulty
     }
 
-    function getRandomRRouletteLottoFunds() public view returns(uint) {
+    function UpdateRandomRRouletteLottoDifficulty(uint new_difficulty) public { //Update difficulty for Randomized Russian Roulette
+        require(msg.sender == lotteryManager); //Can only be updated by owner of contract
+	RandomRRouletteLottoDifficulty=new_difficulty;
+    } 
+    
+    function getRandomRRouletteLottoFunds() public view returns(uint) { //See how much is up for grabs in Randomized Russian Roulette Lotto
         return RandomRRouletteLottoFunds;
     } 
     
+
+    
     function () public payable {
-        // default blank function
+        msg.sender.transfer(msg.value);
     }
     
     function destroy() public  {
